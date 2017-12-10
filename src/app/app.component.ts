44bbe26888c287423a62d3d3430ed0bae131335a
romelgomez <bmxquiksilver7185@gmail.com>
2017-12-10 19:00:11 -0400

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  viewProviders: [ MatIconRegistry ]
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(iconReg: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconReg
+    .addSvgIcon('github', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/github.svg'))
+    .addSvgIcon('google', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/google.svg'))
+    .addSvgIcon('facebook', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/facebook.svg'))
+    .addSvgIcon('twitter', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/twitter.svg'));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { HttpClientModule } from '@angular/common/http';
 
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    // HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
